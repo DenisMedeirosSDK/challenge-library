d7b712e34d37bc2f9f359fe95d224534d15c2648
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-05-14 23:35:54 -0300

    feat: create generic error

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +11,20 @@ app.use(cors());
 app.get("/", (request: Request, response: Response) => {
   return response.end("hello world");
 });
+
+app.use(
+  (err: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "Error",
+      message: "Internal server error",
+    });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);

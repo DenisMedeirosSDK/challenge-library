098d423f16b6d82fdc97f353792bba7f9efad0e7
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-05-17 11:06:47 -0300

    feat: create repository prisma book for save in database and create route http

---
+++ src/controllers/update-book-controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import Joi from "joi";
+import { PrismaBookRepository } from "../repositories/prisma/prisma-book-repository";
+import { PrismaLibraryRepository } from "../repositories/prisma/prisma-library-repository";
+import { UpdateBookUseCase } from "../use-cases/update-book-use-case";
+
+const schema = Joi.object({
+  id: Joi.string().required(),
+  edition: Joi.string().alphanum().min(2).max(4),
+  state: Joi.string().valid("NEW", "USED", "DAMAGED"),
+  library: Joi.string(),
+  libraryBookId: Joi.alternatives().conditional("library", {
+    is: Joi.string().required(),
+    then: Joi.string().required(),
+    otherwise: Joi.string().optional(),
+  }),
+});
+
+export class UpdateBookController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id, edition, state, library, libraryBookId } = request.body;
+
+    try {
+      await schema.validateAsync({
+        id,
+        edition,
+        state,
+        library,
+        libraryBookId,
+      });
+    } catch (error) {
+      throw new Error(`Invalid request: ${error}`);
+    }
+
+    const prismaBookRepository = new PrismaBookRepository();
+    const prismaLibraryRepository = new PrismaLibraryRepository();
+    const updateBookUseCase = new UpdateBookUseCase(
+      prismaBookRepository,
+      prismaLibraryRepository
+    );
+
+    await updateBookUseCase.execute({
+      id,
+      edition,
+      state,
+      library,
+      libraryBookId,
+    });
+
+    return response.end();
+  }
+}
--- src/repositories/prisma/prisma-book-repository.ts
+++ src/repositories/prisma/prisma-book-repository.ts
@@ -1,8 +1,72 @@
-import { CreateBook, ListBook } from "../../dtos/book-dto";
+import {
+  BookState,
+  CreateBook,
+  LibraryBook,
+  ListBook,
+} from "../../dtos/book-dto";
 import { prisma } from "../../libs/prisma";
 import { BookRepository } from "../../repositories/book-repository";
 
 export class PrismaBookRepository implements BookRepository {
+  async findLibraryBookId(
+    libraryBookId: string
+  ): Promise<LibraryBook | null | undefined> {
+    return prisma.libraryBook.findFirst({
+      where: {
+        id: libraryBookId,
+      },
+      select: {
+        id: true,
+        libraryId: true,
+        bookId: true,
+      },
+    });
+  }
+  async update(
+    id: string,
+    edition?: string,
+    state?: BookState,
+    library?: string,
+    libraryBookId?: string
+  ): Promise<void> {
+    if (libraryBookId && library) {
+      await prisma.book.update({
+        where: {
+          id,
+        },
+        data: {
+          edition,
+          state,
+          LibraryBook: {
+            update: {
+              data: {
+                libraryId: library,
+              },
+              where: {
+                id: libraryBookId,
+              },
+            },
+          },
+        },
+      });
+    }
+    await prisma.book.update({
+      where: {
+        id,
+      },
+      data: {
+        edition,
+        state,
+      },
+    });
+  }
+  async findById(id: string): Promise<ListBook | null | undefined> {
+    return prisma.book.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
   async create(book: CreateBook): Promise<void> {
     await prisma.book.create({
       data: {

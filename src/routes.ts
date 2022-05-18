444f1a3f092ffd25ceb25aaa13619cd6c143c9db
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-05-18 20:04:21 -0300

    feat: create route to register new admin

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { CreateAdminController } from "./controllers/create-admin-controller";
 import { CreateBookController } from "./controllers/create-book-controller";
 import { CreateLibraryController } from "./controllers/create-library-controller";
 import { UpdateBookController } from "./controllers/update-book-controller";
@@ -6,9 +7,11 @@ import { UpdateBookController } from "./controllers/update-book-controller";
 const createLibraryController = new CreateLibraryController();
 const createBookController = new CreateBookController();
 const updateBookController = new UpdateBookController();
+const createAdminController = new CreateAdminController();
 
 export const routes = Router();
 
 routes.post("/library", createLibraryController.handle);
 routes.post("/book", createBookController.handle);
 routes.put("/book", updateBookController.handle);
+routes.post("/admin", createAdminController.handle);

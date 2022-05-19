5fefad1d4b38a4f9f47f3100e4a4e7c1508c7f54
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-05-19 13:32:53 -0300

    feat: create http route for send forgot password

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateAdminController } from "./controllers/create-admin-controller";
 import { CreateBookController } from "./controllers/create-book-controller";
 import { CreateLibraryController } from "./controllers/create-library-controller";
 import { DeleteAdminController } from "./controllers/delete-admin-controller";
+import { SendForgotPasswordController } from "./controllers/send-forgot-password-controller";
 import { UpdateBookController } from "./controllers/update-book-controller";
 import { VerifyAccountController } from "./controllers/verify-account.controller";
 
@@ -12,6 +13,7 @@ const updateBookController = new UpdateBookController();
 const createAdminController = new CreateAdminController();
 const verifyAccountController = new VerifyAccountController();
 const deleteAdminController = new DeleteAdminController();
+const sendForgotPasswordController = new SendForgotPasswordController();
 
 export const routes = Router();
 
@@ -19,5 +21,6 @@ routes.post("/library", createLibraryController.handle);
 routes.post("/book", createBookController.handle);
 routes.put("/book", updateBookController.handle);
 routes.post("/admin", createAdminController.handle);
+routes.post("/send-forgot-password", sendForgotPasswordController.handle);
 routes.delete("/library/admin/:libraryId", deleteAdminController.handle);
 routes.get("/user/activate/:token", verifyAccountController.handle);
